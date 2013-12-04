5e87d84ee3f072d1194d3ca5eae3411ffc946dbd
Edward Catmur <ecatmur@ahl.com>
2013-12-04 18:30:17 +0000

    Conditionally initialise ahl logging framework

--- plugins/org.python.pydev/pysrc/pydevconsole.py
+++ plugins/org.python.pydev/pysrc/pydevconsole.py
@@ -245,6 +245,11 @@ def StartServer(host, port, client_port):
 if __name__ == '__main__':
     sys.stdin = BaseStdIn()
 
+    try:
+        import ahl.logging
+    except Exception as ex:
+        sys.stderr.write('Unable to initialise AHL logging framework: %s' % ex)
+
     # http://jira.maninvestments.com/jira/browse/AHLRAP-1421
     import time
     import threading
